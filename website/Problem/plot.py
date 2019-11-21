bba9ceee353ec0742bd435d0cce126ddb14a024b
“Antoine <antoinelouis34@gmail.com>
2019-11-21 20:14:26 +0200

    Update color

--- website/Problem/plot.py
+++ website/Problem/plot.py
@@ -2,15 +2,16 @@ import plotly.graph_objects as go
 import plotly.io as pio
 
 colors = ["#222","#C9FFBF","#F2F8FD","#FBCEB5","#F2F8FD","#FF756B","#F2F8FD"]
+template="plotly_dark"
 
 fig =go.Figure(go.Sunburst(
     labels=["Greenhouse Gas emissions (%)", "Food", "Other", "Animal products", "Other ", "Beef and Lamb", "Other  "],
     parents=["", "Greenhouse Gas emissions (%)", "Greenhouse Gas emissions (%)", "Food", "Food", "Animal products", "Animal products"],
     values=[100, 26, 74, 15, 11, 7.5, 7.5],
     branchvalues="total",
-    marker=dict(colors=colors),
+    marker=dict(colors=colors)
 ))
 
-fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
+fig.update_layout(margin = dict(t=0, l=0, r=0, b=0), template=template)
 pio.write_html(fig, file='index.html', auto_open=True)
 fig.show()
